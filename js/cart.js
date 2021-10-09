f834c9d5e8044ac4826e368e170a8a359b35415f
Rebekka Rustad <79134327+rebekkarustad@users.noreply.github.com>
2021-10-09 22:19:34 +0200

    Details

--- js/cart.js
+++ js/cart.js
@@ -7,3 +7,15 @@ addToCart.addEventListener('click', function(){
     interger +=1;
     int.innerHTML = "(" + interger + ")";
 });
+
+
+
+var addToCartHamburger = document.querySelector("#cartButton");
+
+var intHamburger = document.querySelector("#hamburger-counter");
+var intergerHamburger = 0;
+
+addToCartHamburger.addEventListener('click', function(){
+    intergerHamburger +=1;
+    intHamburger.innerHTML = "(" + intergerHamburger + ")";
+});
